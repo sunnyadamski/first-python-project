700409f1be922a75bd98355ebfaaadebdd0ee939
Adam <sunnyadamski@gmail.com>
2026-09-06 18:34:26 +0100

    Add Lesson 3 loops

---
+++ lessons/lesson_03/loops.py
@@ -0,0 +1,8 @@
+for number in range(1, 11):
+    result = number * 2
+    print(number, "x 2 =", result)
+
+films = ["Inception", "The Matrix", "Interstellar", "The Dark Knight"]
+
+for index, film in enumerate(films):
+    print(f"{index + 1}. {film}")
