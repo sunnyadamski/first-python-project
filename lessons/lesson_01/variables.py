7d005ac416001a21099203764eb42375a7a4ccde
Adam <sunnyadamski@gmail.com>
2026-09-06 18:19:41 +0100

    Add Lesson 1 variables

---
+++ lessons/lesson_01/variables.py
@@ -0,0 +1,20 @@
+name = "Adam"
+age = 25
+height = 1.75
+is_learning_python = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Learning Python:", is_learning_python)
+
+next_year_age = age + 1
+print("Next year I will be:", next_year_age)
+
+favourite_food = "Pizza"
+favourite_number = 7
+likes_coding = True
+
+print("Favourite Food:", favourite_food)
+print("Favourite Number:", favourite_number)
+print("Likes Coding:", likes_coding)
