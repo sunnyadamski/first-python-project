09ae343fe243878125da077ddffd4343baefd815
Adam <sunnyadamski@gmail.com>
2026-09-06 18:28:54 +0100

    Add Lesson 2 decisions

---
+++ lessons/lesson_02/decisions.py
@@ -0,0 +1,10 @@
+age = 25
+
+if age >= 18:
+    print("You are an adult.")
+elif 13 <= age <= 17:
+    print("You are a teenager.")
+else:
+    print("You are a child.")
+
+print("Your age is", age)
